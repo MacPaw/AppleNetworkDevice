dc483cb9447d2c5c2cb314badca72e8d9d0bce9b
Serg Krivoblotsky <kri.sergy@gmail.com>
2022-05-22 12:52:53 +0300

    project init

---
+++ AppleNetworkDevice/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AppleNetworkDevice
+//
+//  Created by Sergii Kryvoblotskyi on 5/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
